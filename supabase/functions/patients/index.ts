f7ca901ad9638dcffb7ad7830856d1a41e5fc09c
Robin Rana <robinrana4r@gmail.com>
2025-07-29 21:35:48 +0600

    feat: create api for assign coach

---
+++ supabase/functions/patients/index.ts
@@ -0,0 +1,12 @@
+import { Hono } from "jsr:@hono/hono";
+import { checkRole } from "../../_shared/middleware.ts";
+import { withCors } from "../../_shared/withCors.ts";
+import assignCoach from "../../controller/patients/assignCoach.ts";
+
+const functionName = "patients";
+const app = new Hono().basePath(`/${functionName}`);
+
+// Routes
+app.use("/assign-coach", checkRole(["admin"]), assignCoach);
+
+Deno.serve(withCors(app));
