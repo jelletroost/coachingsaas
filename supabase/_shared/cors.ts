5f73cc1f17850131600f30b071df066042a693cf
Robin Rana <robinrana4r@gmail.com>
2025-08-01 20:06:30 +0600

    update cors

--- supabase/_shared/cors.ts
+++ supabase/_shared/cors.ts
@@ -1,7 +1,16 @@
-export const corsHeaders = {
-   "Access-Control-Allow-Origin": "http://localhost:3000",
-   "Access-Control-Allow-Credentials": "true",
-   "Access-Control-Allow-Headers":
-      "authorization, x-client-info, apikey, content-type, cookie",
-   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-};
+const allowedOrigins = [
+   "https://dev.coachingsaas.nl", 
+   "https://www.coachingsaas.nl", 
+   "http://localhost:3000"
+ ];
+ 
+ export function getCorsHeaders(request: Request) {
+   const origin = request.headers.get("origin");
+   
+   return {
+     "Access-Control-Allow-Origin": allowedOrigins.includes(origin) ? origin : "null",
+     "Access-Control-Allow-Credentials": "true",
+     "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type, cookie",
+     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+   };
+ }
